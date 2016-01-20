9cec2e61970ed266c4ae6e839d6888485acbb0b8
Viktor Benei <viktor.benei@gmail.com>
2016-01-20 21:28:44 +0100

    godeps

---
+++ Godeps/_workspace/src/github.com/bitrise-io/go-utils/pathutil/pathutil.go
@@ -0,0 +1,112 @@
+package pathutil
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+// IsRelativePath ...
+func IsRelativePath(pth string) bool {
+	if strings.HasPrefix(pth, "./") {
+		return true
+	}
+
+	if strings.HasPrefix(pth, "/") {
+		return false
+	}
+
+	if strings.HasPrefix(pth, "$") {
+		return false
+	}
+
+	return true
+}
+
+// EnsureDirExist ...
+func EnsureDirExist(dir string) error {
+	exist, err := IsDirExists(dir)
+	if !exist || err != nil {
+		return os.MkdirAll(dir, 0777)
+	}
+	return nil
+}
+
+func genericIsPathExists(pth string) (os.FileInfo, bool, error) {
+	if pth == "" {
+		return nil, false, errors.New("No path provided")
+	}
+	fileInf, err := os.Stat(pth)
+	if err == nil {
+		return fileInf, true, nil
+	}
+	if os.IsNotExist(err) {
+		return nil, false, nil
+	}
+	return fileInf, false, err
+}
+
+// IsPathExists ...
+func IsPathExists(pth string) (bool, error) {
+	_, isExists, err := genericIsPathExists(pth)
+	return isExists, err
+}
+
+// IsDirExists ...
+func IsDirExists(pth string) (bool, error) {
+	fileInf, isExists, err := genericIsPathExists(pth)
+	if err != nil {
+		return false, err
+	}
+	if !isExists {
+		return false, nil
+	}
+	if fileInf == nil {
+		return false, errors.New("No file info available.")
+	}
+	return fileInf.IsDir(), nil
+}
+
+// AbsPath expands ENV vars and the ~ character
+//	then call Go's Abs
+func AbsPath(pth string) (string, error) {
+	if pth == "" {
+		return "", errors.New("No Path provided")
+	}
+	if len(pth) >= 2 && pth[:2] == "~/" {
+		pth = strings.Replace(pth, "~/", "$HOME/", 1)
+	}
+	return filepath.Abs(os.ExpandEnv(pth))
+}
+
+// CurrentWorkingDirectoryAbsolutePath ...
+func CurrentWorkingDirectoryAbsolutePath() (string, error) {
+	return filepath.Abs("./")
+}
+
+// UserHomeDir ...
+func UserHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
+}
+
+// NormalizedOSTempDirPath ...
+// Returns a temp dir path. If tmpDirNamePrefix is provided it'll be used
+//  as the tmp dir's name prefix.
+// Normalized: it's guaranteed that the path won't end with '/'.
+func NormalizedOSTempDirPath(tmpDirNamePrefix string) (retPth string, err error) {
+	retPth, err = ioutil.TempDir("", tmpDirNamePrefix)
+	if strings.HasSuffix(retPth, "/") {
+		retPth = retPth[:len(retPth)-1]
+	}
+	return
+}
