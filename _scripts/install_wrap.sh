76ac080918fed0177a7df433892f098a172090eb
Viktor Benei <viktor.benei@gmail.com>
2016-01-25 12:46:28 +0100

    install wrap : silent flag

--- _scripts/install_wrap.sh
+++ _scripts/install_wrap.sh
@@ -5,6 +5,6 @@ temp_dir="$(mktemp -d -t codesigndoc)"
 
 cd $temp_dir
 
-curl -fL https://github.com/bitrise-tools/codesigndoc/releases/download/0.9.2/codesigndoc-Darwin-x86_64 > ./codesigndoc
+curl -sfL https://github.com/bitrise-tools/codesigndoc/releases/download/0.9.2/codesigndoc-Darwin-x86_64 > ./codesigndoc
 chmod +x ./codesigndoc
 ./codesigndoc scan
