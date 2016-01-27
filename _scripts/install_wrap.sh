a40e77d27afadc1c37bab5126dc815f495d5759b
Viktor Benei <viktor.benei@gmail.com>
2016-01-27 11:09:51 +0100

    install wrap fix

--- _scripts/install_wrap.sh
+++ _scripts/install_wrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -x
+set -e
 
 temp_dir="$(mktemp -d -t codesigndoc)"
 
-cd $temp_dir
+cd "$temp_dir"
 
 curl -sfL https://github.com/bitrise-tools/codesigndoc/releases/download/0.9.6/codesigndoc-Darwin-x86_64 > ./codesigndoc
 chmod +x ./codesigndoc
