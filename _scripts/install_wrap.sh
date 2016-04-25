04ef48b931f8020ac0d7ed2778e8ff2a9ae83b30
Viktor Benei <viktor.benei@gmail.com>
2016-04-25 13:52:00 +0200

    install wrap script fix

--- _scripts/install_wrap.sh
+++ _scripts/install_wrap.sh
@@ -7,7 +7,7 @@ codesigndoc_bin_path="${temp_dir}/codesigndoc"
 
 codesigndoc_download_url="https://github.com/bitrise-tools/codesigndoc/releases/download/0.9.8/codesigndoc-Darwin-x86_64"
 echo " * Downloading codesigndoc from (${codesigndoc_download_url}) to (${codesigndoc_bin_path}) ..."
-curl -sfL "$codesigndoc_download_url > "${codesigndoc_bin_path}"
+curl -sfL "$codesigndoc_download_url" > "${codesigndoc_bin_path}"
 echo " * Making it executable ..."
 chmod +x "${codesigndoc_bin_path}"
 echo " * Running codesigndoc scan ..."
