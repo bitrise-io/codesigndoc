7dde72621108fa11778a368a0565b88c5296ff5b
Viktor Benei <viktor.benei@gmail.com>
2016-04-25 14:11:30 +0200

    better log / formatting of install_wrap script

--- _scripts/install_wrap.sh
+++ _scripts/install_wrap.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
-echo " * Creating a temporary directory for codesigndoc ..."
+echo " => Creating a temporary directory for codesigndoc ..."
 temp_dir="$(mktemp -d -t codesigndoc)"
 codesigndoc_bin_path="${temp_dir}/codesigndoc"
 
 codesigndoc_download_url="https://github.com/bitrise-tools/codesigndoc/releases/download/0.9.8/codesigndoc-Darwin-x86_64"
-echo " * Downloading codesigndoc from (${codesigndoc_download_url}) to (${codesigndoc_bin_path}) ..."
+echo " => Downloading codesigndoc from (${codesigndoc_download_url}) to (${codesigndoc_bin_path}) ..."
 curl -sfL "$codesigndoc_download_url" > "${codesigndoc_bin_path}"
-echo " * Making it executable ..."
+echo " => Making it executable ..."
 chmod +x "${codesigndoc_bin_path}"
+echo " => codesigndoc version: $(codesigndoc -version)"
+echo " => Running codesigndoc scan ..."
 echo
-echo " * codesigndoc version:"
-codesigndoc -version
-echo
-echo " * Running codesigndoc scan ..."
 ${codesigndoc_bin_path} scan
