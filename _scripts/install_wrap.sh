3a4a37daedf18666b8a0c517fab0a3e07d759f4b
Viktor Benei <viktor.benei@gmail.com>
2016-06-23 14:45:20 +0200

    install wrap : allow the version to be specified as a param

--- _scripts/install_wrap.sh
+++ _scripts/install_wrap.sh
@@ -5,7 +5,13 @@ echo " => Creating a temporary directory for codesigndoc ..."
 temp_dir="$(mktemp -d -t codesigndoc)"
 codesigndoc_bin_path="${temp_dir}/codesigndoc"
 
-codesigndoc_download_url="https://github.com/bitrise-tools/codesigndoc/releases/download/0.9.8/codesigndoc-Darwin-x86_64"
+version_to_use="0.9.8"
+if [ "$1" != "" ] ; then
+    version_to_use="$1"
+fi
+echo " => Downloading version: ${version_to_use}"
+
+codesigndoc_download_url="https://github.com/bitrise-tools/codesigndoc/releases/download/${version_to_use}/codesigndoc-Darwin-x86_64"
 echo " => Downloading codesigndoc from (${codesigndoc_download_url}) to (${codesigndoc_bin_path}) ..."
 curl -sfL "$codesigndoc_download_url" > "${codesigndoc_bin_path}"
 echo " => Making it executable ..."
