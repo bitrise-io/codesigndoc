87f7028644df99c54a12cbacc5049c874e1972f4
Viktor Benei <viktor.benei@gmail.com>
2016-09-22 22:03:01 +0200

    minimal cleanup

--- cmd/xamarin.go
+++ cmd/xamarin.go
@@ -19,15 +19,4 @@ var xamarinCmd = &cobra.Command{
 
 func init() {
 	scanCmd.AddCommand(xamarinCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// xamarinCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// xamarinCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
