edf3724e55ce6db289eacf407e5515dca0989dbb
Viktor Benei <viktor.benei@gmail.com>
2016-09-28 00:36:33 +0200

    install wrap for 0.9.12

--- _scripts/install_wrap.sh
+++ _scripts/install_wrap.sh
@@ -5,7 +5,7 @@ echo " => Creating a temporary directory for codesigndoc ..."
 temp_dir="$(mktemp -d -t codesigndoc)"
 codesigndoc_bin_path="${temp_dir}/codesigndoc"
 
-version_to_use="0.9.11"
+version_to_use="0.9.12"
 if [ "$1" != "" ] ; then
     version_to_use="$1"
 fi
@@ -13,6 +13,8 @@ if [ ! -z "${CODESIGNDOC_VERSION}" ] ; then
     version_to_use="${CODESIGNDOC_VERSION}"
 fi
 echo " => Downloading version: ${version_to_use}"
+
+scan_command_to_use="xcode"
 
 codesigndoc_download_url="https://github.com/bitrise-tools/codesigndoc/releases/download/${version_to_use}/codesigndoc-Darwin-x86_64"
 echo " => Downloading codesigndoc from (${codesigndoc_download_url}) to (${codesigndoc_bin_path}) ..."
@@ -22,4 +24,4 @@ chmod +x "${codesigndoc_bin_path}"
 echo " => codesigndoc version: $(${codesigndoc_bin_path} -version)"
 echo " => Running codesigndoc scan ..."
 echo
-${codesigndoc_bin_path} scan
+${codesigndoc_bin_path} scan ${scan_command_to_use}
