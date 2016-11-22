b07c4a07e54b82e0c89b27a0948cf2dd0fdf299b
Viktor Benei <viktor.benei@gmail.com>
2016-11-22 15:43:30 +0100

    codesigndoc version --full
    with the --full flag it now prints the Go, Arch and OS versions where the binary was built

--- cmd/version.go
+++ cmd/version.go
@@ -2,9 +2,14 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/bitrise-tools/codesigndoc/version"
 	"github.com/spf13/cobra"
+)
+
+var (
+	isFullVersionPrint = false
 )
 
 // versionCmd represents the version command
@@ -14,9 +19,17 @@ var versionCmd = &cobra.Command{
 	Long:  `Prints version number`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(version.VERSION)
+
+		if isFullVersionPrint {
+			fmt.Println()
+			fmt.Println("go: " + runtime.Version())
+			fmt.Println("arch: " + runtime.GOARCH)
+			fmt.Println("os: " + runtime.GOOS)
+		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
+	versionCmd.Flags().BoolVar(&isFullVersionPrint, "full", false, "Full version")
 }
