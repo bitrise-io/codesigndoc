2dbfb2e6beb1caba385266504aa29340c8ab50b1
Viktor Benei <viktor.benei@gmail.com>
2017-03-30 12:06:59 +0200

    CHANGELOG & install wraps for v1.0.0

--- _scripts/install_wrap-xamarin.sh
+++ _scripts/install_wrap-xamarin.sh
@@ -5,7 +5,7 @@ echo " => Creating a temporary directory for codesigndoc ..."
 temp_dir="$(mktemp -d -t codesigndocXXXXXX)"
 codesigndoc_bin_path="${temp_dir}/codesigndoc"
 
-version_to_use="0.9.18"
+version_to_use="1.0.0"
 if [ "$1" != "" ] ; then
     version_to_use="$1"
 fi
