b42f78c222b1742151efe77e4496d6ac4f4d243e
Krisztián  Gödrei <godreikrisztian@gmail.com>
2018-05-18 14:42:41 +0200

    profile test

---
+++ codesigndoc/profile_test.go
@@ -0,0 +1,56 @@
+package codesigndoc
+
+import (
+	"testing"
+	"time"
+
+	"github.com/bitrise-tools/go-xcode/profileutil"
+	"github.com/stretchr/testify/require"
+)
+
+func createTime(t *testing.T, timeStr string) time.Time {
+	date, err := time.Parse("2006.01.02", timeStr)
+	require.NoError(t, err)
+	return date
+}
+
+func timeToString(date time.Time) string {
+	return date.Format("2006.01.02")
+}
+
+func TestFilterLatestProfiles(t *testing.T) {
+	profiles := []profileutil.ProvisioningProfileInfoModel{
+		{
+			Name:           "Profile 1",
+			BundleID:       "*",
+			ExpirationDate: createTime(t, "2017.11.01"),
+		},
+		{
+			Name:           "Profile 1",
+			BundleID:       "*",
+			ExpirationDate: createTime(t, "2017.12.01"),
+		},
+		{
+			Name:           "Profile 2",
+			BundleID:       "io.bitrise",
+			ExpirationDate: createTime(t, "2017.10.01"),
+		},
+	}
+
+	filtered := filterLatestProfiles(profiles)
+	require.Equal(t, 2, len(filtered))
+
+	desiredProfileExpireMap := map[string]bool{
+		"2017.12.01": false,
+		"2017.10.01": false,
+	}
+	for _, profile := range filtered {
+		expire := timeToString(profile.ExpirationDate)
+		if _, ok := desiredProfileExpireMap[expire]; ok {
+			desiredProfileExpireMap[expire] = true
+		}
+	}
+	for _, found := range desiredProfileExpireMap {
+		require.True(t, found)
+	}
+}
