8eca216292c88a9a82b758093362991f4aefbb2f
Akos Birmacher <akos.birmacher@bitrise.io>
2018-05-25 11:38:25 +0200

    Error log updated for: "If the xcode archive is success, we have the archive required codesigning files. Then codesigndoc asks for the distribution method, if method selected, which codesigning files are not exist on the developer's machine it mentions the fail, and asks if do you want to export other files? If no selected it finishes with strange error: no ipa export code sign groups collected"

--- codesigndoc/codesigngroup.go
+++ codesigndoc/codesigngroup.go
@@ -184,7 +184,8 @@ func collectIpaExportCodeSignGroups(archive xcarchive.IosArchive, installedCerti
 
 		if len(filteredCodeSignGroups) == 0 {
 			fmt.Println()
-			log.Errorf(collectCodesigningFilesInfo)
+			log.Errorf("🚨  Could not find any valid code signing file for the selected export method:")
+			log.Warnf(collectCodesigningFilesInfo)
 			fmt.Println()
 			fmt.Println()
 			question := "Do you want to collect another ipa export code sign files"
