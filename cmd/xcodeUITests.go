6d421f23ffb6d3aa10da86f751c954e7f759f5d0
Akos Birmacher <akos.birmacher@bitrise.io>
2019-01-14 17:07:01 +0100

    filterScheme by BuildFortesting == YES

--- cmd/xcodeUITests.go
+++ cmd/xcodeUITests.go
@@ -2,13 +2,19 @@ package cmd
 
 import (
 	"fmt"
+	"path"
+	"path/filepath"
 	"strings"
+
+	"github.com/bitrise-tools/xcode-project/xcscheme"
 
 	"github.com/bitrise-io/go-utils/colorstring"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/goinp/goinp"
 	"github.com/bitrise-tools/codesigndoc/xcode"
 	"github.com/bitrise-tools/go-xcode/utility"
+	"github.com/bitrise-tools/xcode-project/xcodeproj"
+	"github.com/bitrise-tools/xcode-project/xcworkspace"
 	"github.com/spf13/cobra"
 )
 
@@ -63,23 +69,44 @@ the one you usually open in Xcode, then hit Enter.
 	log.Debugf("projectPath: %s", projectPath)
 	xcodeUITestsCmd.ProjectFilePath = projectPath
 
+	workspace, err := xcworkspace.Open(projectPath)
+	if err != nil {
+		return fmt.Errorf("Failed to open workspace (%s), error: %s", projectPath, err)
+	}
+
 	schemeToUse := paramXcodeScheme
 	if schemeToUse == "" {
 		fmt.Println()
 		log.Printf("🔦  Scanning Schemes ...")
-		schemes, err := xcodeUITestsCmd.ScanSchemes()
+		schemesByContainer, err := workspace.Schemes()
 		if err != nil {
 			return ArchiveError{toolXcode, "failed to scan Schemes: " + err.Error()}
 		}
+
+		var schemes []xcscheme.Scheme
+		var schemeNames []string
+		for container, containerSchemes := range schemesByContainer {
+			log.Warnf(container)
+			if strings.ToLower(path.Base(container)) != "pods.xcodeproj" {
+				schemes = append(schemes, containerSchemes...)
+			}
+		}
+
+		schemes = filterScheme(schemes)
+
+		for _, scheme := range schemes {
+			schemeNames = append(schemeNames, scheme.Name)
+		}
+
 		log.Debugf("schemes: %v", schemes)
 
 		if len(schemes) == 0 {
 			return ArchiveError{toolXcode, "no schemes found"}
 		} else if len(schemes) == 1 {
-			schemeToUse = schemes[0]
+			schemeToUse = schemes[0].Name
 		} else {
 			fmt.Println()
-			selectedScheme, err := goinp.SelectFromStringsWithDefault("Select the Scheme you usually use in Xcode", 1, schemes)
+			selectedScheme, err := goinp.SelectFromStringsWithDefault("Select the Scheme you usually use in Xcode", 1, schemeNames)
 			if err != nil {
 				return fmt.Errorf("failed to select Scheme: %s", err)
 			}
@@ -95,4 +122,84 @@ the one you usually open in Xcode, then hit Enter.
 	}
 
 	return nil
+}
+
+func filterScheme(schemes []xcscheme.Scheme) []xcscheme.Scheme {
+	var schemesWithTest []xcscheme.Scheme
+	for _, scheme := range schemes {
+		for _, e := range scheme.BuildAction.BuildActionEntries {
+			if e.BuildForTesting == "YES" {
+				schemesWithTest = append(schemesWithTest, scheme)
+				break
+			}
+		}
+
+	}
+	return schemesWithTest
+}
+
+func findBuiltProject(pth, schemeName, configurationName string) (xcodeproj.XcodeProj, string, error) {
+	var scheme xcscheme.Scheme
+	var schemeContainerDir string
+
+	if xcodeproj.IsXcodeProj(pth) {
+		project, err := xcodeproj.Open(pth)
+		if err != nil {
+			return xcodeproj.XcodeProj{}, "", err
+		}
+
+		var ok bool
+		scheme, ok = project.Scheme(schemeName)
+		if !ok {
+			return xcodeproj.XcodeProj{}, "", fmt.Errorf("no scheme found with name: %s in project: %s", schemeName, pth)
+		}
+		schemeContainerDir = filepath.Dir(pth)
+	} else if xcworkspace.IsWorkspace(pth) {
+		workspace, err := xcworkspace.Open(pth)
+		if err != nil {
+			return xcodeproj.XcodeProj{}, "", err
+		}
+
+		var containerProject string
+		scheme, containerProject, err = workspace.Scheme(schemeName)
+		if err != nil {
+			return xcodeproj.XcodeProj{}, "", fmt.Errorf("no scheme found with name: %s in workspace: %s", schemeName, pth)
+		}
+		schemeContainerDir = filepath.Dir(containerProject)
+	} else {
+		return xcodeproj.XcodeProj{}, "", fmt.Errorf("unknown project extension: %s", filepath.Ext(pth))
+	}
+
+	if configurationName == "" {
+		configurationName = scheme.TestAction.BuildConfiguration
+	}
+
+	if configurationName == "" {
+		return xcodeproj.XcodeProj{}, "", fmt.Errorf("no configuration provided nor default defined for the scheme's (%s) archive action", schemeName)
+	}
+
+	var archiveEntry xcscheme.BuildActionEntry
+	for _, entry := range scheme.BuildAction.BuildActionEntries {
+		if entry.BuildForArchiving != "YES" || !entry.BuildableReference.IsAppReference() {
+			continue
+		}
+		archiveEntry = entry
+		break
+	}
+
+	if archiveEntry.BuildableReference.BlueprintIdentifier == "" {
+		return xcodeproj.XcodeProj{}, "", fmt.Errorf("archivable entry not found")
+	}
+
+	projectPth, err := archiveEntry.BuildableReference.ReferencedContainerAbsPath(schemeContainerDir)
+	if err != nil {
+		return xcodeproj.XcodeProj{}, "", err
+	}
+
+	project, err := xcodeproj.Open(projectPth)
+	if err != nil {
+		return xcodeproj.XcodeProj{}, "", err
+	}
+
+	return project, scheme.Name, nil
 }
