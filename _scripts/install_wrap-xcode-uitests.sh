fe96c9308d4fba8bd6332e4c42b3184b183b250e
Akos Birmacher <akos.birmacher@bitrise.io>
2019-02-26 09:19:39 +0100

    feat: add install_wap-xcode-uitests script

---
+++ _scripts/install_wrap-xcode-uitests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo " => Creating a temporary directory for codesigndoc ..."
+temp_dir="$(mktemp -d -t codesigndocXXXXXX)"
+codesigndoc_bin_path="${temp_dir}/codesigndoc"
+
+version_to_use="2.2.0"
+if [ "$1" != "" ] ; then
+    version_to_use="$1"
+fi
+if [ ! -z "${CODESIGNDOC_VERSION}" ] ; then
+    version_to_use="${CODESIGNDOC_VERSION}"
+fi
+echo " => Downloading version: ${version_to_use}"
+
+scan_command_to_use="xcodeuitests"
+
+codesigndoc_download_url="https://github.com/bitrise-tools/codesigndoc/releases/download/${version_to_use}/codesigndoc-Darwin-x86_64"
+echo " => Downloading codesigndoc from (${codesigndoc_download_url}) to (${codesigndoc_bin_path}) ..."
+curl -fL --progress-bar --output "${codesigndoc_bin_path}" "$codesigndoc_download_url"
+echo " => Making it executable ..."
+chmod +x "${codesigndoc_bin_path}"
+echo " => codesigndoc version: $(${codesigndoc_bin_path} version)"
+echo " => Running codesigndoc scan ..."
+echo
+${codesigndoc_bin_path} scan ${scan_command_to_use}
